ae7f9340ad38a49a7216be13f2f0e263fb543842
Odylon-Kleber <odylon.kleber@gmail.com>
2025-07-29 11:53:02 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
+
 import streamlit as st
+import pandas as pd
+import numpy as np
+
+
 
 st.header("Minha dashboard")
 st.text("Meu texto")
 
 # Análise de dados
-import pandas as pd
 dados_questionario = pd.read_csv(r"C:\Users\Odylon\Desktop\Questionário TEA - Respostas.csv", encoding='utf-8')
 
 st.data_editor(dados_questionario)
+
+
